15f62a320d93881e7fb84f9ad01d96b5bde85833
Diego Rodríguez <diegorbaquero@gmail.com>
2016-09-27 16:46:34 -0500

    HTML funcional

--- CubeSummation.js
+++ CubeSummation.js
@@ -1,29 +1,29 @@
-let input = '2\n' +
-  '4 5\n' +
-  'UPDATE 2 2 2 4\n' +
-  'QUERY 1 1 1 3 3 3\n' +
-  'UPDATE 1 1 1 23\n' +
-  'QUERY 2 2 2 4 4 4\n' +
-  'QUERY 1 1 1 3 3 3\n' +
-  '2 4\n' +
-  'UPDATE 2 2 2 1\n' +
-  'QUERY 1 1 1 1 1 1\n' +
-  'QUERY 1 1 1 2 2 2\n' +
-  'QUERY 2 2 2 2 2 2'
+function log (text) {
+  console.log(text)
+  if (document) document.querySelector('#output').innerText += text + '\n'
+}
 
 function correr (input) {
-  let inputArray = input.split('\n')
-  let lineaActual = 0
-  let casos = parseInt(inputArray[ lineaActual++ ])
+  try {
+    if (document) document.querySelector('#output').innerText += ''
+    let inputArray = input.split('\n')
+    if (inputArray.length < 3) throw new Error('Texto de entrada inválido')
+    let lineaActual = 0
+    let casos = parseInt(inputArray[ lineaActual++ ])
 
-  for (let caso = 0; caso < casos; caso++) {
-    let informacionCaso = inputArray[ lineaActual++ ].split(' ').map((n) => parseInt(n))
-    let tamaño = informacionCaso[ 0 ]
-    let operaciones = informacionCaso[ 1 ]
-    let matriz = crearMatriz(tamaño)
-    for (let operacion = 0; operacion < operaciones; operacion++) {
-      correrOperacion(inputArray[ lineaActual++ ], matriz)
+    for (let caso = 0; caso < casos; caso++) {
+      let informacionCaso = inputArray[ lineaActual++ ].split(' ').map((n) => parseInt(n))
+      let tamaño = informacionCaso[ 0 ]
+      let operaciones = informacionCaso[ 1 ]
+      let matriz = crearMatriz(tamaño)
+      for (let operacion = 0; operacion < operaciones; operacion++) {
+        let operacion = inputArray[ lineaActual++ ]
+        correrOperacion(operacion, matriz)
+      }
     }
+  } catch (err) {
+    console.error(err)
+    document.querySelector('#error').innerText = err
   }
 }
 
@@ -45,39 +45,44 @@ function correrOperacion (operacion, matriz) {
   let operacionArray = operacion.split(' ')
   let tipo = operacionArray[ 0 ]
   if (tipo === 'UPDATE') {
-    const coord = {
-      x: parseInt(operacionArray[ 1 ]) - 1,
-      y: parseInt(operacionArray[ 2 ]) - 1,
-      z: parseInt(operacionArray[ 3 ]) - 1
-    }
-    const valor = parseInt(operacionArray[ 4 ])
-    matriz[ coord.x ][ coord.y ][ coord.z ] = valor
+    correrUpdate(operacionArray, matriz)
   } else if (tipo === 'QUERY') {
-    const coordInicio = {
-      x: parseInt(operacionArray[ 1 ]) - 1,
-      y: parseInt(operacionArray[ 2 ]) - 1,
-      z: parseInt(operacionArray[ 3 ]) - 1
-    }
-    const coordFinal = {
-      x: parseInt(operacionArray[ 4 ]) - 1,
-      y: parseInt(operacionArray[ 5 ]) - 1,
-      z: parseInt(operacionArray[ 6 ]) - 1
-    }
-    let suma = 0
-    for (let x = coordInicio.x; x <= coordFinal.x; x++) {
-      for (let y = coordInicio.y; y <= coordFinal.y; y++) {
-        for (let z = coordInicio.z; z <= coordFinal.z; z++) {
-          suma += matriz[x][y][z]
-        }
-      }
-    }
-    console.log(suma)
+    correrQuery(operacionArray, matriz)
   } else {
     throw new Error('Tipo de operación inválido')
   }
 }
 
-correr(input)
+function correrUpdate (valores, matriz) {
+  const coord = {
+    x: parseInt(valores[ 1 ]) - 1,
+    y: parseInt(valores[ 2 ]) - 1,
+    z: parseInt(valores[ 3 ]) - 1
+  }
+  const valor = parseInt(valores[ 4 ])
+  matriz[ coord.x ][ coord.y ][ coord.z ] = valor
+}
 
+function correrQuery (valores, matriz) {
+  const coordInicio = {
+    x: parseInt(valores[ 1 ]) - 1,
+    y: parseInt(valores[ 2 ]) - 1,
+    z: parseInt(valores[ 3 ]) - 1
+  }
+  const coordFinal = {
+    x: parseInt(valores[ 4 ]) - 1,
+    y: parseInt(valores[ 5 ]) - 1,
+    z: parseInt(valores[ 6 ]) - 1
+  }
+  let suma = 0
+  for (let x = coordInicio.x; x <= coordFinal.x; x++) {
+    for (let y = coordInicio.y; y <= coordFinal.y; y++) {
+      for (let z = coordInicio.z; z <= coordFinal.z; z++) {
+        suma += matriz[x][y][z]
+      }
+    }
+  }
+  log(suma)
+}
 
 
